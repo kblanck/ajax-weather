dc0699d5637190ad900db1afb42451b2eaa56416
Kim Blanck <kimblanck@kims-air.home>
2020-12-05 17:31:45 -0500

    modals working

--- first-project/app.js
+++ first-project/app.js
@@ -1,6 +1,27 @@
 $(() => {
+
+    // const $seeMyJacket = $('#submit');
+    // const $modal = $('.modal');
+    // const $closeWindow = $('#close')
+    //
+    // const openModal = () => {
+    //   $modal.css('display', 'block')
+    // }
+  $modalDiv = $('#modal').css('display', 'none');
+
+  const openModal = () => {
+    $modalDiv.css('display', 'block')
+  }
+  const closeModal = () => {
+    $modalDiv.css('display','none');
+  }
+
   $('.button').on('click', (e) => {
+
     e.preventDefault();
+
+    openModal();
+
     const $zip = $('#zip').val();
 
   $.ajax({
@@ -8,23 +29,26 @@ $(() => {
     type: "GET",
   }).then(
     (data) => {
+      console.log(data);
       const temp = Math.round(data.main.feels_like);
+      const rain = data.weather[0].main
+      console.log(rain);
       if (temp <= 10) {
-        console.log(`Oh my God, it feels like ${temp}° outside. Stay inside!`);
+        $modalDiv.append(`Oh my God, it feels like ${temp}° outside. Stay inside!`);
       } else if (temp > 10 && temp <= 25) {
-          console.log(`Yikes, it feels like ${temp}° outside. Grab that sleeping bag coat.`);
+          $modalDiv.append(`Yikes, it feels like ${temp}° outside. Grab that sleeping bag coat.`);
         } else if (temp > 25 && temp <= 39) {
-          console.log(`Yeah, we wouldn't say it's warm (because it feels like ${temp}° out there). Get that down coat on.`);
+          $modalDiv.append(`Yeah, we wouldn't say it's warm (because it feels like ${temp}° out there). Get that down coat on.`);
         } else if (temp > 39 && temp <= 49) {
-          console.log(`Not so bad! It feels like ${temp}° out there, so grab that wool coat.`);
+          $modalDiv.append(`Not so bad! It feels like ${temp}° out there, so grab that wool coat.`);
         } else if (temp > 49 && temp <= 59) {
-          console.log(`Oooh. In our opinion, ${temp}° is the PERFECT walking temp. Break out that bomber today!`);
+          $modalDiv.append(`Oooh. In our opinion, ${temp}° is the PERFECT walking temp. Break out that bomber today!`);
         } else if (temp > 59 && temp <= 68) {
-          console.log(`We love a balmy ${temp}°. All you need is a denim jacket today, honey!`);
+          $modalDiv.append(`We love a balmy ${temp}°. All you need is a denim jacket today, honey!`);
         } else if (temp > 68 && temp <= 74) {
-          console.log(`It's ${temp}° where you are?? Girl, all you need is a hoodie! Go enjoy your day!`);
+          $modalDiv.append(`It's ${temp}° where you are?? Girl, all you need is a hoodie! Go enjoy your day!`);
         } else if (temp > 74 && temp <= 95) {
-          console.log(`Wow, at ${temp}°, winter is not known where you are. Throw on a T-shirt and enjoy your year-round summer!`);
+          $modalDiv.append(`Wow, at ${temp}°, winter is not known where you are. Throw on a T-shirt and enjoy your year-round summer!`);
         }
       })
     }
