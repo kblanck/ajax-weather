dcb60f80af6504b9dcea00d3650e06a7ea26e982
Kim Blanck <kimblanck@kims-air.home>
2020-12-07 14:10:20 -0500

    took out rain bugs

--- app.js
+++ app.js
@@ -28,22 +28,20 @@ $(() => {
   }).then(
     (data) => {
       const temp = Math.round(data.main.feels_like);
-      // const isItRaining = () => {
-      //   if (data.weather[0].main == 'Rain'){
-      //     console.log('Raining');
-      //   } else {
-      //     console.log('Not raining');
-      //   }
-      // }
-      // isItRaining();
+      // const rain = data.weather[0].main;
+      // console.log(data);
+      const isItRaining = () => {
+        if (data.weather[0].main == 'Rain'){
+          console.log('Raining');
+        } else {
+          console.log('Not raining');
+        }
+      }
+      isItRaining();
       if (temp <= 10) {
         $modalContentDiv.append('<img id="onesie" src="./imgs/onesie.png">').append(`Oh my God, it feels like ${temp}° outside. Stay inside and maybe wear this onesie.`)
       } else if (temp > 10 && temp <= 25) {
           $modalContentDiv.append('<img id="sleeping-bag" src="./imgs/sleeping-bag.png">').append(`Yikes, it feels like ${temp}° outside. Grab that sleeping bag coat.`);
-          { if (data.weather[0].main === 'Rain') {
-            $modalContentDiv.append(` And bring an umbrella.`);
-            }
-          }
         } else if (temp > 25 && temp <= 39) {
           $modalContentDiv.append('<img id="down" src="./imgs/down.png">').append(`Yeah, we wouldn't say it's warm (because it feels like ${temp}° out there). Get that down coat on.`);
           { if (data.weather[0].main === 'Rain') {
@@ -56,31 +54,12 @@ $(() => {
             $modalContentDiv.append(` And bring an umbrella.`);
             }
           }
-
         } else if (temp > 49 && temp <= 59) {
-          { if (data.weather[0].main !== 'Rain') {
-            $modalContentDiv.append('<img id="bomber" src="./imgs/bomber.png">').append(`Oooh. In our opinion, ${temp}° is the PERFECT walking temp. Break out that bomber today!`);
-            }
-          }
-          { if (data.weather[0].main === 'Rain') {
-            $modalContentDiv.append('<img id="raincoat" src="./imgs/raincoat.png">').append(`${temp}° doesn't make for a super cold day, but break out that raincoat. It's coming down!`);
-            }
-          }
-
+          $modalContentDiv.append('<img id="bomber" src="./imgs/bomber.png">').append(`Oooh. In our opinion, ${temp}° is the PERFECT walking temp. Break out that bomber today!`);
         } else if (temp > 59 && temp <= 68) {
           $modalContentDiv.append('<img id="denim" src="./imgs/denim.png">').append(`We love a balmy ${temp}°. All you need is a denim jacket today, honey!`);
-          { if (data.weather[0].main !== 'Rain') {
-            $modalContentDiv.append('<img id="denim" src="./imgs/denim.png">').append(`We love a balmy ${temp}°. All you need is a denim jacket today, honey!`);
-            }
-          }
-          { if (data.weather[0].main === 'Rain') {
-            $modalContentDiv.append('<img id="raincoat" src="./imgs/raincoat.png">').append(`At ${temp}°, it's not cold out there, but make sure you don that raincoat today. It's coming down!`);
-            }
-          }
-
         } else if (temp > 68 && temp <= 74) {
           $modalContentDiv.append('<img id="hoodie" src="./imgs/hoodie.png">').append(`It's ${temp}° where you are?? Girl, all you need is a hoodie! Go enjoy your day!`);
-          
         } else if (temp > 74 && temp <= 95) {
           $modalContentDiv.append('<img id="t-shirt" src="./imgs/t-shirt.png">').append(`Wow, at ${temp}°, winter is not known where you are. Throw on a T-shirt and enjoy your year-round summer!`);
         }
